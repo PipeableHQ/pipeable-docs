e631bf1bb32da210156fc1ce199be667ff58b52a
pipeable-com <153856921+pipeable-com@users.noreply.github.com>
2024-03-18 22:07:53 -0700

    Delete .docusaurus directory

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/juanlozano/dev/pipeable/pipeable-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/juanlozano/dev/pipeable/pipeable-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/juanlozano/dev/pipeable/pipeable-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/juanlozano/dev/pipeable/pipeable-docs/src/css/custom.css"),
-];
